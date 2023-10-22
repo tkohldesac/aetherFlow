89475b225741e6eb2536e66453d6c8c7e038c7ab
tkohldesac <topher@kohldesac.com>
2023-10-21 22:55:24 -0700

    updates

--- src/aetherFlow.js
+++ src/aetherFlow.js
@@ -3,7 +3,7 @@
 export function render(root, DOMElement) {
     const element = document.createElement(root.tag);
     DOMElement.appendChild(element);
-
+    
     // Need logic to handle adding children
     // Need to add attributes
     // Need recursion
