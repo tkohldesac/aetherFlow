19cb5844afa1739f44cb578b475d6e6884cc1f20
tkohldesac <topher@kohldesac.com>
2023-11-11 10:02:52 -0800

    empty commit

--- src/aetherflow.js
+++ src/aetherflow.js
@@ -1,4 +1,3 @@
-
 export function render(vdomNode, DOMElement) {
 console.log(vdomNode.tag);
   // Creating a new node based on the tag of the incoming node. First thing that comes in should be something like an h1. Next console.log should be the first child and so on. Lots of [object Objects]
@@ -6,6 +5,40 @@ console.log(vdomNode.tag);
   // Add that new node to the parent. When this cycles through, the DOMElement will be the parent from before so the first pass adds the h1 to the root, the child gets added to the h1, etc. until all of the children are added to all of the parents.
   DOMElement.appendChild(newNode);
 
+  Object.entries(vdomNode.props).forEach((entry) => {
+    const attributeName = entry[0];
+    const attributeValue = entry[1];
+
+    // If the attributeName isn't 'children', setAttribute on the new node to the name and value above. 
+    if (attributeName !== "children" ){
+
+      if (attributeName === "style") {
+        // here inline styles are applied individually
+        // since the `.styles` attribute of DOM elements isn't a regular object, doing something like, `newElement.setAttribute("styles", { color: "red" });` does not work
+        Object.entries(attributeValue).forEach(([styleName, styleValue]) => {
+          newNode.style[styleName] = styleValue;
+        });
+      }
+      // newNode.setAttribute(attributeName, attributeValue);
+      // Qualifier for different attributes  like 'startsWith('on')s
+
+      if (attributeName.startsWith("on")) {
+
+        newNode.addEventListener(attributeName.toLowerCase().slice(2), attributeValue);
+      } 
+
+      else if (attributeName === 'disabled') {
+           if(attributeValue) {
+              newNode.setAttribute(attributeName, attributeValue)
+           }
+           
+        }
+        
+
+      // Otherwise, for each child...
+    }
+  });
+  
   // This is a boolean, if there are children within vdomNode.props, it continues the logic
   if (vdomNode.props.children) {
     // This sets the children to an array if it isn't already. 
@@ -14,28 +47,11 @@ console.log(vdomNode.tag);
       : [vdomNode.props.children];
 
       // Create arrays. attributeName and attributeValue to be added to the nodes
-    Object.entries(vdomNode.props).forEach((entry) => {
-      const attributeName = entry[0];
-      const attributeValue = entry[1];
-
-      // If the attributeName isn't 'children', setAttribute on the new node to the name and value above. 
-      if (attributeName !== "children" ){
-        newNode.setAttribute(attributeName, attributeValue);
+ {
+        children.forEach((child) => {
         
-        // Otherwise, for each child...
-      } else {children.forEach((child) => {
-        // If it's a function,
-        if (typeof child === "function" && child !== null) {
-          console.log("It's a function, buddeh");
-          const freshVdomNode = vdomNode.tag(vdomNode.props);
-          render(freshVdomNode, anchorElement);
-  
         // If it's a string...
-       }else if (attributeName.startsWith("on")) {
-
-        newElement.addEventListener(attributeName.toLowerCase().slice(2), attributeValue);
-
-      } else if (typeof child === "string") {
+          if (typeof child === "string" || typeof child === "number") {
             console.log("It's a string!");
             console.log(`String: ${child}`);
             // Create a text node with the value of the child.
@@ -52,7 +68,7 @@ console.log(vdomNode.tag);
           // Additional type checks can go here
         });
       }
-    });
+
   }
 }
 
@@ -60,6 +76,6 @@ console.log(vdomNode.tag);
 // Need to add attributes - inline styles, eventListeners, everything else that isn't children (disabled={false})
 // Need recursion - done
 // Logic for rerendering - done
-// Attributes
-// Functions
-// State
+// Attributes - done
+// Functions that return JSX
+// State 
